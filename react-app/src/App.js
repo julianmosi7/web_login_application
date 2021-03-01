7a0c10b3c4cecd9c56ad05084e0a8531b8a3630f
Julian Moshammer <moshammer.ju@gmail.com>
2021-03-01 08:27:26 +0100

    init

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './Login'
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Login</h1>
+        <Login />
+      </main>
+    </>
+  )
+}
+
+export default App;
---
+++ react-app/src/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export default function Login() {
+    return (
+       <>
+        <label>Username:</label>
+        <input type="text"></input>
+        <br></br>
+        <label>Password:</label>
+        <input type="password"></input>
+       </>
+    )
+}
