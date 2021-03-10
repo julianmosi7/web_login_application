6f1905280626049ac66eb0251e96f54e60176251
Julian <moshammer.ju@gmail.com>
2021-03-10 15:31:34 +0100

    push cleaned repo

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,21 +4,8 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const name = 'Julian'
-  const x = false
-
-  return (
-    <div className='container'>
-      <h1>Hello From React</h1>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
-    </div>
-  )
-}
-
-export default App;
-
-/* 
-<Router>
+  return(
+    <Router>
       <div className="App">
         <main>
           <Route path="/login" component={Login} />
@@ -26,4 +13,7 @@ export default App;
         </main>
       </div>
     </Router>
- */
+  )
+}
+
+export default App;
--- react-app/src/Login.js
+++ react-app/src/Login.js
@@ -1,17 +1,18 @@
 import "./Login.css";
 import React, { useState } from 'react'
+import Button from '@material-ui/core/Button';
+import { TextField } from "@material-ui/core";
 
 const Login = () => {
     return (
         <div className="login">
-            <h1>Login</h1>
-            <label>Username:</label>
-            <input type="text"></input>
-            <br></br>
-            <label>Password:</label>
-            <input type="password"></input>
-            <br></br>
-            <button>Submit</button>
+            <h1>Login</h1> 
+            <p>Log in to your account</p>
+            <TextField id="standard-basic" label="username"></TextField>
+            <TextField id="standard-basic" label="password"></TextField>
+            <Button variant="contained" color="primary">
+                Submit
+            </Button>
         </div>
     )
 }
